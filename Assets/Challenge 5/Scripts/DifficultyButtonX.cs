2eff9764a552a6a73bac3205d7f296303a30fabb
a57654 <a57654@alunos.ipb.pt>
2025-06-10 16:13:21 +0100

    Fim Challenge 5

---
+++ Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;  // valor 1, 2 ou 3 atribuído no Inspector
+
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked with difficulty " + difficulty);
+        gameManagerX.StartGame(difficulty); // chama StartGame passando a dificuldade
+    }
+}
+
